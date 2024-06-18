0023fd4f9e3aaf86168d35c07e5233963417b641
norboka <90785798+norboka@users.noreply.github.com>
2024-06-18 10:18:22 +0200

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -1,7 +1,31 @@
-document.getElementById("scrollToBottomBtn").addEventListener("click", function() {
-    window.scrollTo({
-        top: document.body.scrollHeight,
-        behavior: "smooth"
+document.addEventListener('DOMContentLoaded', function () {
+    const button = document.getElementById('scroll-button');
+    const arrow = document.getElementById('arrow');
+    const documentHeight = document.documentElement.scrollHeight;
+        const windowHeight = window.innerHeight;
+    const scrollThreshold = (documentHeight - windowHeight) / 2;
+
+    button.addEventListener('click', function () {
+        if (window.scrollY < scrollThreshold) {
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            });
+        } else {
+            window.scrollTo({
+                top: document.documentElement.scrollHeight,
+                behavior: 'smooth'
+            });
+        }
+    });
+
+    window.addEventListener('scroll', function () {
+        if (window.scrollY < scrollThreshold) {
+            arrow.src = 'Images/Oldal/noun-arrow-19208020.png';
+            button.setAttribute('title', 'Görgessen az oldal tetejére');
+        } else {
+            arrow.src = 'Images/Oldal/noun-arrow-1920802.png';
+            button.setAttribute('title', 'Görgessen az oldal aljára');
+        }
     });
 });
-
